49c1c9dd1043b6d8aaae46fd26e74900d17fc9ed
cave johnson <166028235+Sillylittleguy1@users.noreply.github.com>
2024-10-13 13:30:15 -0500

    Create startup.lua
    boot file

---
+++ startup.lua
@@ -0,0 +1,33 @@
+term.clear()
+term.setCursorPos(1,1)  -- Ensure printing starts at the top left corner
+check = True
+local time = textutils.formatTime(os.time(utc))  
+print("   singularity OS[1.0b]", time)
+print("                                 @         ")
+print("                      @@::@    :           ")
+print("              :    @@::@@                  ")
+print("                 @@@::@          @@@       ")
+print("               @@:-@     @@@@::::@@@::@    ")
+print("              @@:     @@@@:@@          @   ")
+print("            @ @:     @@@::@   @         @  ")
+print("              @:    @@@  @@::  @    @      ")
+print("                @    @  ::@@  @@@    :@    ")
+print("            @         @   @::@@@     :@ @  ")
+print("             @          @@:@@@@     :@@    ")
+print("              @::@@@::::@@@@     @-:@@     ")
+print("                 @@@          @::@@@       ")
+print("                            @@::@@    :    ")
+print("                     :    @::@@            ")
+print("                   @                       ")
+print("[                                                 ]")
+term.setCursorPos(2,18)
+textutils.slowPrint("=================================================")
+if fs.exists("/main") == False then
+  term.setCursorPos(1,18)
+  print("             Error, missing file main.lua                 ")
+  check = False
+end
+sleep(2)
+if check == True then
+shell.run("main")
+end
