1ba49758dd8be878118dac0e1dfcc94808f00704
cave johnson <166028235+Sillylittleguy1@users.noreply.github.com>
2024-10-14 13:39:29 -0500

    Update main.lua
    updated the wrong code last time

--- main.lua
+++ main.lua
@@ -1,43 +1,121 @@
 term.clear()
-term.setCursorPos(1,2)
-print("Proceeding with the operation will overwrite any files with the same name, do you understand? Y/N")
-local a = read()
+local button = require("button")
+os.pullEvent = os.pullEventRaw
+local pos = {0, 0}
+w, h = term.getSize()
 
-if a == "Y" then
-    local username = "Sillylittleguy1"
-    local repo = "singularity-OS"
-    local branch = "main"
-    local files = {
-        "button.lua",
-        "startup.lua",
-        "main.lua",
-    }
-    local baseURL = "https://raw.githubusercontent.com/" .. username .. "/" .. repo .. "/" .. branch .. "/"
-    
-    local function downloadFile(file)
-        local url = baseURL .. file
-        print("Downloading: " .. file)
-        local response = http.get(url)
-        if response then
-            local content = response.readAll()
-            response.close()
-            local f = fs.open(file, "w")
-            f.write(content)
-            f.close()
-            print("Downloaded: " .. file)
-        else
-            print("Failed to download: " .. file)
-        end
-    end
+-- Time update flag
+local keepUpdatingTime = true
 
-    for _, file in ipairs(files) do
-        downloadFile(file)
-    end
-    print("All files downloaded.")
-    sleep(1)
-    term.clear()
-  else
-  print("Operation canceled.")
-  sleep(1)
-  term.clear()
+-- Lookup table for movement
+local LookupMove = {
+    [keys.up] = {0, 1},
+    [keys.down] = {0, -1},
+    [keys.left] = {-1, 0},
+    [keys.right] = {1, 0},
+    [keys.w] = {0, 1},
+    [keys.a] = {-1, 0},
+    [keys.s] = {0, -1},
+    [keys.d] = {1, 0},
+    [keys.enter] = "Enter"
+}
+
+local function compareVectors(v1, v2)
+    return v1[1] == v2[1] and v1[2] == v2[2]
 end
+
+local function clampPosition(pos)
+    if pos[1] < 0 then
+        pos[1] = 0
+    elseif pos[1] > 0 then
+        pos[1] = 0
+    end
+    if pos[2] > 0 then
+        pos[2] = 0
+    elseif pos[2] < -3 then
+        pos[2] = -3
+    end
+    return pos
+end
+
+local function addv(v1, v2)
+    return {v1[1] + v2[1], v1[2] + v2[2]}
+end
+
+local function updateTime()
+    while true do
+        local cx, cy = term.getCursorPos()
+        local time = textutils.formatTime(os.time(), true) -- 24-hour format
+        term.setCursorPos(1, 1)
+        term.clearLine()
+        term.write("singularity OS [v1.0b] " .. time)
+        term.setCursorPos(cx, cy)
+        sleep(1)
+    end
+end
+
+
+local function center()
+    x = w / 2
+    y = h / 2
+end
+
+local function menus(n)
+    if n == 1 then
+        center()
+        term.setCursorPos(x - 4, y - 2)
+        print("Start Menu ")
+        button.make("[System]", " System ", {0, 0}, pos, x - 4, y)
+        button.make("[Terminal]", " Terminal ", {0, -1}, pos, x - 4, y + 1)
+        button.make("[Programs]", " Programs ", {0, -2}, pos, x - 4, y + 2)
+        button.make("[Shutdown]", " Shutdown ", {0, -3}, pos, x - 4, y + 3)
+    elseif n == 2 then
+        center()
+        term.setCursorPos(x - 4, y - 2)
+        print("System Menu")
+        button.make("[Update]", " Update ", {0, 0}, pos, x - 4, y)
+        button.make("[Settings]", " Settings ", {0, -1}, pos, x - 4, y + 1)
+        button.make("[Uninstall]", " Uninstall ", {0, -2}, pos, x - 4, y + 2)
+        button.make("[<Back]", " <Back  ", {0, -3}, pos, x - 3, y + 3)
+    end
+end
+
+local m = 1
+parallel.waitForAny(
+    function()
+        while true do
+            menus(m)
+            local event, key = os.pullEvent("key")
+            local move = LookupMove[key]
+            if move ~= nil and move ~= "Enter" then
+                pos = clampPosition(addv(pos, move))
+            elseif move == "Enter" then
+                if m == 1 then
+                    if compareVectors(pos, {0, 0}) then
+                        m = 2
+                    elseif compareVectors(pos, {0, -1}) then
+                        term.clear()
+                        term.setCursorPos(1, 1)
+                        break
+                    elseif compareVectors(pos, {0, -2}) then
+                        shell.run("programs/")
+                    elseif compareVectors(pos, {0, -3}) then
+                        os.shutdown()
+                    end
+                elseif m == 2 then
+                    if compareVectors(pos, {0, 0}) then
+                        shell.run("install.lua")
+                    elseif compareVectors(pos, {0, -1}) then
+                        print("Opening Settings...")
+                    elseif compareVectors(pos, {0, -2}) then
+                        print("Uninstalling...")
+                    elseif compareVectors(pos, {0, -3}) then
+                        m = 1
+                    end
+                end
+                pos = {0, 0}
+            end
+        end
+    end,
+    updateTime
+)
