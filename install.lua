594ad55e8a12fb07c152db5a94d55e64ff13ec57
cave johnson <166028235+Sillylittleguy1@users.noreply.github.com>
2024-10-14 01:03:59 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,37 +1,39 @@
-local username = "Sillylittleguy1"
-local repo = "singularity-OS"
-local branch = "main"  
+term.clear()
+print("Proceeding with the operation will overwrite any files with the same name, do you understand? Y/N")
+local a = read()
 
-local files = {
-  "button.lua",
-  "startup.lua",
-  "main.lua",
-  
-}
-
-local baseURL = "https://raw.githubusercontent.com/" .. username .. "/" .. repo .. "/" .. branch .. "/"
-
-local function downloadFile(file)
-  local url = baseURL .. file
-  print("Downloading: " .. file)
-  
-  local response = http.get(url)
-  if response then
-    local content = response.readAll()
-    response.close()
+if a == "Y" then
+    local username = "Sillylittleguy1"
+    local repo = "singularity-OS"
+    local branch = "main"
+    local files = {
+        "button.lua",
+        "startup.lua",
+        "main.lua",
+    }
+    local baseURL = "https://raw.githubusercontent.com/" .. username .. "/" .. repo .. "/" .. branch .. "/"
     
-    local f = fs.open(file, "w")
-    f.write(content)
-    f.close()
+    local function downloadFile(file)
+        local url = baseURL .. file
+        print("Downloading: " .. file)
+        local response = http.get(url)
+        if response then
+            local content = response.readAll()
+            response.close()
+            local f = fs.open(file, "w")
+            f.write(content)
+            f.close()
+            print("Downloaded: " .. file)
+        else
+            print("Failed to download: " .. file)
+        end
+    end
 
-    print("Downloaded: " .. file)
-  else
-    print("Failed to download: " .. file)
-  end
+    for _, file in ipairs(files) do
+        downloadFile(file)
+    end
+
+    print("All files downloaded.")
+else
+    print("Operation canceled.")
 end
-
-for _, file in ipairs(files) do
-  downloadFile(file)
-end
-
-print("All files downloaded.")
