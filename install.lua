3c8dc82c7648c70bc96c69d0e096f3f21cd5fecd
cave johnson <166028235+Sillylittleguy1@users.noreply.github.com>
2024-10-13 22:50:16 -0500

    Create install.lua
    allows users to grab the entire repo

---
+++ install.lua
@@ -0,0 +1,37 @@
+local username = "Sillylittleguy1"
+local repo = "singularity-OS"
+local branch = "main"  
+
+local files = {
+  "button.lua",
+  "startup.lua",
+  "main.lua",
+  
+}
+
+local baseURL = "https://raw.githubusercontent.com/" .. username .. "/" .. repo .. "/" .. branch .. "/"
+
+local function downloadFile(file)
+  local url = baseURL .. file
+  print("Downloading: " .. file)
+  
+  local response = http.get(url)
+  if response then
+    local content = response.readAll()
+    response.close()
+    
+    local f = fs.open(file, "w")
+    f.write(content)
+    f.close()
+
+    print("Downloaded: " .. file)
+  else
+    print("Failed to download: " .. file)
+  end
+end
+
+for _, file in ipairs(files) do
+  downloadFile(file)
+end
+
+print("All files downloaded.")
